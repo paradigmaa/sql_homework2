742e0743f49be1b61c6b144c234396a9cf630ade
Alexandr Arkhipov <Alexandr14m.a@gmail.com>
2025-08-27 18:44:52 +0300

    Initial commit

---
+++ src/main/java/school/sorokin/javacore/sql2/App.java
@@ -0,0 +1,13 @@
+package school.sorokin.javacore.sql2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
